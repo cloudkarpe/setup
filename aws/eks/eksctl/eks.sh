bfee72786158dfaf200ab20933666935f005613c
Amit Karpe <amitkarpe@gmail.com>
2019-12-22 19:58:06 +0800

    EKS from GUI

--- aws/eks/eksctl/eks.sh
+++ aws/eks/eksctl/eks.sh
@@ -1,17 +1,22 @@
-curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
-sudo mv aws-iam-authenticator /usr/local/bin
+dir=~/.local/bin
+mkdir -p ${dir}
+curl -o ${dir}/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
+#sudo cp ~/.local/bin/aws-iam-authenticator /usr/local/bin
+#cp aws-iam-authenticator ~/.local/bin
 
-curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl
+curl -o ${dir}/kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl
 #curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.8/2019-08-14/bin/linux/amd64/kubectl
 #curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.10/2019-08-14/bin/linux/amd64/kubectl
 
-sudo mv kubectl /usr/local/bin
+#sudo cp kubectl /usr/local/bin
+#cp kubectl ~/.local/bin
 
-
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_Linux_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_Linux_amd64.tar.gz" | tar xz -C ${dir}/
+#sudo cp /tmp/eksctl /usr/local/bin
+#cp eksctl ~/.local/bin
 
 echo ". <(eksctl completion bash)" >> ~/.profile
 echo ". <(kubectl completion bash)" >> ~/.profile
 
-sudo chmod +x /usr/local/bin/*
+#sudo chmod +x /usr/local/bin/*
+chmod +x ~/.local/bin/* 
---
+++ aws/eks/gui/Readme.md
@@ -0,0 +1,22 @@
+# EKS GUI installation
+
+Some of the topics we'll be covering in this video are:
+
+    Creating the EKS service role
+    Creating the VPC infrastructure using CloudFormation [link](https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2019-11-15/amazon-eks-vpc-sample.yaml)
+    Creating a cluster in the AWS Management Console
+    Configuring kubectl for EKS
+    Configuring aws-iam-authenticator
+
+
+
+
+Source: https://linuxacademy.com/cp/courses/lesson/course/3584/lesson/2/module/293
+
+Some of the topics we'll be covering in this video are:
+    [Cloudformation Template](https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-11-07/amazon-eks-nodegroup.yaml)
+    Launching EKS worker nodes [ami](https://docs.aws.amazon.com/eks/latest/userguide/eks-optimized-ami.html)
+    Deploying the Kubernetes dashboard
+
+optimized-ami/1.14 + us-east-1
+ami-087a82f6b78a07557
